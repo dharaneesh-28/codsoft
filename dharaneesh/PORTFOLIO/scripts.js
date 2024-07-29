8860c909024266a7a3e47aee2b7b466666e19300
dharaneesh-28 <dharaneesh852@gmail.com>
2024-07-29 20:26:03 +0530

    Add files via upload

---
+++ dharaneesh/PORTFOLIO/scripts.js
@@ -0,0 +1,16 @@
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const message = document.getElementById('message').value;
+    
+    console.log('Name:', name);
+    console.log('Email:', email);
+    console.log('Message:', message);
+    
+    alert('Message sent successfully!');
+    
+    
+    event.target.reset();
+});
